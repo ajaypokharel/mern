de663ec1b30515098572ac88322ff1c6719d1a85
Ajay Pokharel <ajaypokharel8@gmail.com>
2022-11-26 22:28:27 -0600

    minor changes with MongoDB Password

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
+const express = require("express");
+const cors = require("cors");
 
-/** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
- * import express */
-const express = require('express');
-const cors = require('cors');
 // middleware
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-// `npm install mongoose`
+// Connect MongoDB using mongoose
 const mongoose = require("mongoose");
 const options = {
-    keepAlive: true,
-    connectTimeoutMS: 10000,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  keepAlive: true,
+  connectTimeoutMS: 10000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
-const adminPass = encodeURIComponent("hello");
-const dbUrl = `mongodb+srv://ajayfied:${adminPass}@cluster0.ijwa2eo.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URL =
+  "mongodb+srv://mern-api:jOyvLH5AcxLqXmSa@mern.syrqciq.mongodb.net/?retryWrites=true&w=majority";
 
 // Mongo DB connection
-mongoose.connect(dbUrl, options, (err) => {
+mongoose.connect(MONGO_URL, options, (err) => {
   if (err) console.log(err);
 });
 
@@ -31,63 +29,58 @@ db.once("open", function () {
   console.log("Mongo DB Connected successfully");
 });
 
-// Schema for Book
+// Schema for posts
 let Schema = mongoose.Schema;
 let postSchema = new Schema(
   {
     id: {
       type: Number,
     },
-    title: {
+    content: {
       type: String,
-    },
-    description: {
-      type: String,
-    },
-    date: {
-      type: Date,
     },
   },
   { timestamps: true }
 );
-let PostModel = mongoose.model("post", postSchema);
+
+// Post Model
+let PostModel = mongoose.model("posts", postSchema);
 
 app.get("/", (req, res) => {
   res.send("Your are lucky!! server is running...");
 });
 
-/** GET API: GETs Books from DB and returns as response */
-app.get("/post", async (req, res) => {
+/**GET API: GETs Posts from DB and returns as response */
+app.get("/posts", async (req, res) => {
   try {
     let posts = await PostModel.find();
-    res.status(200).json({
+    res.json({
       status: 200,
       data: posts,
     });
-  } catch (err) {
-    res.status(400).json({
+  } catch (error) {
+    res.json({
       status: 400,
-      message: err.message,
+      message: error.message,
     });
   }
 });
 
 /** POST API: Gets new book info from React and adds it to DB */
-app.post("/post", async (req, res) => {
+app.post("/posts", async (req, res) => {
   const inputPost = req.body;
-  console.log(inputPost);
+
   try {
-    console.log("input Book:", inputBook);
     let post = new PostModel(inputPost);
     post = await post.save();
-    res.status(200).json({
+    res.json({
       status: 200,
       data: post,
     });
-  } catch (err) {
-    res.status(400).json({
+  } catch (error) {
+    res.json({
       status: 400,
-      message: err.message,
+      message: error.message,
     });
   }
 });
@@ -97,26 +90,26 @@ app.post("/post", async (req, res) => {
  * Sends 500 if there is an error while saving data to DB
  * Sends 200 if deleted successfully
  */
-app.delete("/books/:postId", async (req, res) => {
+app.delete("/posts/:postId", async (req, res) => {
   try {
     let post = await PostModel.findByIdAndRemove(req.params.postId);
     if (post) {
-      res.status(200).json({
+      res.json({
         status: 200,
-        message: "Post deleted successfully",
+        message: "Book deleted successfully",
       });
     } else {
-      res.status(400).json({
+      res.json({
         status: 400,
-        message: "No Post found",
+        message: "No Book found",
       });
     }
   } catch (err) {
-    res.status(400).json({
+    res.json({
       status: 400,
       message: err.message,
     });
   }
 });
 
-app.listen(3001);
+app.listen(8080);
